48ff1abdf1dcef29b84575e40c0d68caf7a9b809
aaleiva93 <aaaleiva93@gmail.com>
2016-04-14 17:47:53 +0200

    Initial commit

---
+++ app/src/main/java/leiva/metrotransmobile/MainActivity.java
@@ -0,0 +1,31 @@
+package leiva.metrotransmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button conectar;
+    private EditText usuario, contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        conectar = (Button)findViewById(R.id.button_conectar);
+        usuario = (EditText)findViewById(R.id.editText_nombre);
+        contraseña = (EditText)findViewById(R.id.editText_contraseña);
+
+        conectar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Usuario Correcto", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
